7cf6f79c59331dd406b49e944f4935220198a4b5
tanzilbm <tanzilbm6@gmail.com>
2020-08-09 13:31:13 +0530

    code to generate grid

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -1,12 +1,21 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Grid : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject GridBackground;
+    public GameObject small_grid;
+    private ValueSet[,] arr = new ValueSet[9, 9];
     void Start()
     {
-        
+        for(int i = 0; i<9; i++)
+        {
+            for(int j = 0;j<9;j++)
+            {
+                arr[i, j] = Instantiate(small_grid, Vector3.zero, Quaternion.identity, GridBackground.transform).GetComponent<ValueSet>();
+            }
+        }
     }
 }
